780f59fa50b95558eeb4536807f049fb938c27b7
shun7603 <summersao7603@icloud.com>
2025-05-28 00:40:47 +0900

    JSコード学習
    addEventListenerの使い方について学習中
    要素のイベント発火の際にアクションが実行された場合にfunctionを使ってイベントの内容を設定する内容に取り組んでいる

---
+++ index.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style")
+  })
+
+
+  pullDownButton.addEventListener('click', function(){
+    console.log("click OK")
+  })
+})
